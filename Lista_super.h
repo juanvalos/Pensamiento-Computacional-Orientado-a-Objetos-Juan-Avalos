eca54195e44264df229ee56e9de659fbe51fcfc0
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-11-17 21:30:39 -0600

    Update Lista_super.h

--- Lista_super.h
+++ Lista_super.h
@@ -1,74 +1,14 @@
 #include <string>
 
-class Alimentos {
+class Lista_super {
+    
     private:
-        string nombre;
-        float precio;
-
-    public:
-        
-        string getnombre() {
-            return nombre;
-        }
-
-        float getPrecio() {
-            return precio;
-        }
-        
-        void setNombre(string nom) {
-            nombre = nom;
-        }
-
-        void setPrecio(int pre) {
-            precio = pre;
-        }
-        
-        void printData (){
-             cout << " " << endl;
-             cout << "Alimento" << endl;
-             cout << "Nombre:  " << nombre << endl;
-             cout << "Precio:  " << precio << endl;
-        }
-};
-
-class Bebidas {
-    private:
-        string nombre;
-        float precio;
-
-    public:
-        string getNombre() {
-            return nombre;
-        }
-
-        float getPrecio() {
-            return precio;
-        }
-        
-        void setNombre(string nom) {
-            nombre = nom;
-        }
-
-        void setPrecio(int pre) {
-            precio = pre;
-        }
-
-        void printData (){
-             cout << " " << endl;
-             cout << "Bebida" << endl;
-             cout << "Nombre:  " << nombre << endl;
-             cout << "Precio:  " << precio << endl;
-        }
-};
-
-class ProductosLimp {
-    private:
-        string nombre;
-        float precio;
+        string nombre ;
+        float precio ;
     
     public:
-        string getNombre() {
-            return nombre;
+        string getnombre() {
+        return nombre;
         }
 
         float getPrecio() {
@@ -81,12 +21,15 @@ class ProductosLimp {
 
         void setPrecio(int pre) {
             precio = pre;
+        } 
+
+        void sumatoriaPrecios() {
+
         }
 
-        void printData (){
-             cout << " " << endl;
-             cout << "Producto de limpieza" << endl;
-             cout << "Nombre:  " << nombre << endl;
-             cout << "Precio:  " << precio << endl;
+        void contProductos() {
+
         }
+
+
 };
