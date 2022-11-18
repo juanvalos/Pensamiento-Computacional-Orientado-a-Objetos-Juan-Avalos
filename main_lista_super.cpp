2299af08848b65cf6d04d7f7df3e2acf50ea3f63
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-11-17 21:31:26 -0600

    Update main_lista_super.cpp

--- main_lista_super.cpp
+++ main_lista_super.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 #include "Lista_super.h"
+#include "Alimentos.h"
+#include "Bebidas.h"
+#include "ProductosLimpieza.h"
 
 int main() {
     Alimentos jamon;
     
     jamon.setNombre("Paquete de Jamon");
     jamon.setPrecio(42);
+    jamon.setCalorias(1100);
+    jamon.setPeso (300);
+    jamon.setFechaCad ("12 de Diciembre");
     jamon.printData();
 
 
@@ -15,15 +21,19 @@ int main() {
     
     coca_cola.setNombre("Coca Cola");
     coca_cola.setPrecio(42);
+    coca_cola.setVolumen(700);
+    coca_cola.setFechaCad ("2023");
     coca_cola.printData();
 
 
-    ProductosLimp jabon_roma;
+    ProductosLimpieza jabon_roma;
     
-    jabon_roma.setNombre("Bolsa de Jabón Roma");
+    jabon_roma.setNombre("Jabon liquido");
     jabon_roma.setPrecio(70);
+    jabon_roma.setVolumen(20000);
+    jabon_roma.setUso ("Lavar ropa");
     jabon_roma.printData();
 
 
     return 0;
-}  
+} 
