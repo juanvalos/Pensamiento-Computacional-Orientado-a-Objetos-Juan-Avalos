d445261e81e32d09447bcf5c723dc2103a221eab
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-11-28 19:48:14 -0600

    Update and rename Lista_super.h to Productos.h

--- Productos.h
+++ Productos.h
@@ -1,35 +1,56 @@
+#ifndef PRODUCTOS_H
+#define PRODUCTOS_H
+
+using namespace std;
+#include <iostream>
 #include <string>
 
-class Lista_super {
+class Productos {
     
-    private:
+    protected:
         string nombre ;
         float precio ;
     
     public:
+        
+        // Constructores
+        Productos(string nom, float prec) {
+            nombre = nom ;
+            precio = prec ;
+        }
+
         string getnombre() {
         return nombre;
         }
+
 
         float getPrecio() {
             return precio;
         }
         
+
         void setNombre(string nom) {
             nombre = nom;
         }
+
 
         void setPrecio(int pre) {
             precio = pre;
         } 
 
+
         void sumatoriaPrecios() {
 
         }
+
 
         void contProductos() {
 
         }
 
-
+        virtual void productoAgregado() {
+            cout << "El articulo ha sido agregado al almacen ";
+        } 
 };
+
+#endif
