8f3caeef50b921cbf7ca9a4f34c00895c51e319e
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-12-01 00:14:00 -0600

    Update Productos.h

--- Productos.h
+++ Productos.h
@@ -5,13 +5,20 @@ using namespace std;
 #include <iostream>
 #include <string>
 
+/*
+Esta clase es la clase madre para todo tipo de productos, esta tiene como atributos "nombre" y "precio", ya
+que todos los artículos dentro de un super mercado tienen esas 2 carácterísticas. Usaremos herencia para poder 
+heredar estos 2 atributos y el Método abstracto llamado "PrintData". Elegí usar un método virtual para este ya que este
+metodo va a imprimir toda la informacion del producto, y debido a que cada producto tiene diferentes caracteristicas,
+se tiene que desarrollar este metodo en cada una de las clases de cada tipo de productos.
+*/
 class Productos {
     
-    protected:
+    protected:                  // Agregamos estos 2 atributos en "protected" para que así las puedan heredar las clases hijas.
         string nombre ;
         float precio ;
     
-    public:
+    public:                     // Agregamos estos metodos en "public" para que sean accesados desde el main o por otra clase.
         
         // Constructores
         Productos(string nom, float prec) {
@@ -19,6 +26,7 @@ class Productos {
             precio = prec ;
         }
 
+        // Agregamos Getters
         string getnombre() {
         return nombre;
         }
@@ -28,28 +36,19 @@ class Productos {
             return precio;
         }
         
-
+        // Agregamos Setters
         void setNombre(string nom) {
             nombre = nom;
         }
-
 
         void setPrecio(int pre) {
             precio = pre;
         } 
 
-
-        void sumatoriaPrecios() {
-
-        }
-
-
-        void contProductos() {
-
-        }
-
-        virtual void productoAgregado() {
-            cout << "El articulo ha sido agregado al almacen ";
+        // Agregamos el metodo virtual "printData" que sera desarrollado en cada clase hija
+        virtual void printData() {       
+            cout << "Nombre: "<< nombre<< endl;
+            cout << "Precio: "<< nombre<< endl;
         } 
 };
 
