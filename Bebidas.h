4212b84cdd1a91a9f5053893a9f1b4bdc2370bc2
juanvalos <111295746+juanvalos@users.noreply.github.com>
2022-12-01 00:13:26 -0600

    Update Bebidas.h

--- Bebidas.h
+++ Bebidas.h
@@ -6,30 +6,29 @@ using namespace std;
 
 # include "Productos.h"
 
-class Bebidas : public Productos {
+/*
+Esta clase es una clase hija y recibe herencia por la clase "Productos". Esto significa que esta contiene los metodos
+y atributos que la clase madre tiene, ademas de los que esta clase tiene por aparte. Debido a que esta clase es para clasificar
+bebidas, esta tambien tendra volumen y fecha de caducidad. El metodo abstracto "printData" se desarrolla aqui imprimiendo los
+4 datos que va a contener un atributo de tipo Bebida.
+*/
 
+class Bebidas : public Productos {   // En esta linea de codigo estamos aplicando herencia a la clase
     private:
-        int volumen ;
-        string fecha_caducidad ;
+        int volumen ;               // Aqui ya escribimos los atributos que la bebida va a tener en "private" debido a que esta
+        string fecha_caducidad ;    // esta es ua clase hija y no aplica herencia a otras clases.
     
     public:
       
         // Constructores
         Bebidas (string nom , float prec , int vol, string fecha_cad ) : 
+        
         Productos(nom, prec) {
             volumen = vol;
             fecha_caducidad = fecha_cad;
         }
 
-        //Getters
-        string getnombre() {
-            return nombre;
-        }
-
-        float getPrecio() {
-            return precio;
-        }
-
+        // Agregamos Getters
         int getVolumen() {
             return volumen;
         }
@@ -39,15 +38,7 @@ class Bebidas : public Productos {
         }
         
 
-        //Setters
-        void setNombre(string nom) {
-            nombre = nom;
-        }
-
-        void setPrecio(int pre) {
-            precio = pre;
-        } 
-
+        // Agregamos Setters
         void setvolumen(int vol) {
             volumen = vol;
         } 
@@ -56,9 +47,14 @@ class Bebidas : public Productos {
             fecha_caducidad = fecha;
         } 
         
-        //Metodos abstractos
-        void productoAgregado() {
-            cout << "La Bebida ha sido agregada al Almacen" << endl;
+        // Desarrollamos el metodo abstracto "printData" conforme a los atributos de la clase hija
+        void printData (){
+            cout << " " << endl;
+            cout << "Bebida" << endl;
+            cout << "Nombre:  " << nombre << endl;
+            cout << "Precio:  " << precio << endl;
+            cout << "Volumen:  " << volumen << endl;
+            cout << "Fecha de Caducidad:  " << fecha_caducidad << endl;
         }
 };
 
